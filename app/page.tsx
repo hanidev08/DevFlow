5cc0eaf163bce15110bbfcdaf7b0f77245dde0b9
hanidev08 <hanidev08@gmail.com>
2024-11-08 17:10:22 +0100

    Tailwind CSS Setup & Theming

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" text-3xl text-black font-black">
+    <div className=" text-3xl text-primary-500 font-black">
       Welcome to the world of Next.js
     </div>
   );
