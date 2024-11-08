7edbd868723a7906fc2d8460ba8afda434010eb4
hanidev08 <hanidev08@gmail.com>
2024-11-08 17:24:07 +0100

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" text-3xl text-primary-500 font-black">
+    <div className="h1-bold font-space-grotesk">
       Welcome to the world of Next.js
     </div>
   );
