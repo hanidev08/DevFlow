1442fa40d13f7a8c404efaa52f56d559f30acad0
hanidev08 <hanidev08@gmail.com>
2024-11-10 11:48:00 +0100

    Set up Light & Dark Theme, Shadcn, Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="h1-bold font-space-grotesk">
-      Welcome to the world of Next.js
-    </div>
+    <>
+      <h2 className="h1-bold font-space-grotesk">
+        Welcome to the world of Next.js
+      </h2>
+    </>
   );
 }
